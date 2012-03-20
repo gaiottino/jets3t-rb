c780f212b6919ddb9a61f300d8ce8576c551bf33
Guy Boertje <guyboertje@gmail.com>
2012-03-20 13:14:26 +0000

    use uuid suffix for test bucket name

--- spec/get_put_spec.rb
+++ spec/get_put_spec.rb
@@ -4,8 +4,8 @@ require_relative 'spec_helper'
 module JetS3t
 
   describe "JetS3t get/put" do
-    
-    let(:bucket_name)       { 'jets3t-spec-1' }
+
+    let(:bucket_name)       { test_bucket_name }
     let(:service)           { RestS3Service.new(build_credentials) }
     let(:spec_bucket)       { service.bucket(bucket_name) }
     let(:resource)          { File.open File.join(resource_dir, "silence-44-s.flac") }
@@ -29,7 +29,7 @@ module JetS3t
       subject           { results }
 
       it "after put S3Object contains metadata and the list has the file" do
-        
+
         metadata = subject[:uploaded][0].metadata_map
         metadata["Content-Length"].should == size
 
@@ -47,12 +47,12 @@ module JetS3t
       subject { results }
 
       it "after get, the S3Object returned is equal to the resource" do
-        
+
         key = subject[:uploaded][0].key
 
         subject[:downloaded][0].key.should == key
         subject[:downloaded][0].verifyData(java.io.File.new(resource.path)).should be_true
-        
+
       end
     end
   end
